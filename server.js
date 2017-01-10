c5490506aa233150354e79021a6dc29331159337
Richelle2016 <ridjeukam@yahoo.fr>
2017-01-10 17:23:53 +0100

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [/*{
@@ -32,12 +34,15 @@ app.get('/todos', function(req, res){
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id,10);
 
-	var found ;
+	var found = _.findWhere(todos,{id: todoId});
+	/*var found ;
 	todos.forEach(function(todo){
 		if(todo.id === todoId){
 			found = todo;
 		}
-	});
+	});*/
+
+
 	if(found){
 		res.json(found);
 	}else {
@@ -46,9 +51,21 @@ app.get('/todos/:id', function(req, res){
 
 });
 
+
+
 //POST /todos
 app.post('/todos', function(req, res) {
-	var body = req.body;
+	//var body = req.body;
+
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed)  || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+	
+	//set body.description to be trimed value
+	body.description = body.description.trim();
+
 
 	// add id field
 	body.id = todoNextId;
